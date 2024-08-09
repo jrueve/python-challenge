edea8ce652853becf18a14134c4f5dfdc1175e6b
jrueve <141786894+jrueve@users.noreply.github.com>
2024-08-08 23:41:39 -0500

    next commit

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,44 +1,61 @@
-import os, csv
+#import necessary systems
+import os
+import csv
 
+#select correct csv from resource file
 csvpath = os.path.join("Resources","budget_data.csv")
+
 
 total = 0
 date = []
 profit = []
 
+#open csv read through each line and define the list or variables needed
 with open(csvpath, newline="", encoding='utf-8') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     #print(csvreader)
     csv_header = next(csvreader)
     #print(f"CSV Header: {csv_header}")
 
+#loop through each row in csv to count months, count the total for the financial analysis
     totalmonths = 0  
     datalist = list(csvreader)
     for row in csvreader: 
         totalmonths += 1
 
         total = total + int(row[1])
+        #get date for each row
         date.append(row[0])
+        #get profit number of each row
         profit.append(int(row[1]))
-    
+        print(row[0])
+        #define profit start value
     beginprofit = int(datalist[0][1])
+    #set final profit value to 0
     finalprofit =  0
     diff = 0
     monthlydiff = []
 
+#loop through copy of csvreader
     for i in range(1, len(datalist)):
+        #get final profit from list
         finalprofit = int(datalist[i][1])
+        #check the difference from the final to begininng profit of month
         diff = finalprofit - beginprofit
+        #add to list
         monthlydiff.append(diff)
+        #change beginning profit to be used for next month value
         beginprofit = int(datalist[i][1])
+    #find average of monthly difference
     avgChange = round(sum(monthlydiff)/len(monthlydiff),2)
+    #find greatest increase and decrease seen betwwen months of profit
     greatestincrease = max(monthlydiff)
     greatestdecrease = min(monthlydiff)
-    greatestincreasedate = date[greatestincrease]
-    greatestdecreasedate = date[greatestdecrease]
+    #find date the greatest increase and decrease of profit took place
+    #greatestincreasedate = date[greatestincrease]
+    #greatestdecreasedate = date[greatestdecrease]
 
-
-
+    #print our findings
     print(totalmonths)
     print(total)
     print(beginprofit)
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,44 +1,42 @@
+#import necessary systems
 import os, csv
 
+#select correct csv from resource file
 csvpath = os.path.join("Resources","election_data.csv")
 
-candidatelist = []
-totalvotes = 0
-
+#open csv read through each line and define the list or variables needed
 with open(csvpath, newline="", encoding='utf-8') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     #print(csvreader)
     csv_header = next(csvreader)
     #print(f"CSV Header: {csv_header}")
-    vote0 = 0
-    vote1 = 0
-    vote2 = 0
+
     totalvotes = 0
     candidatelist = []
     votelist = []
+    votecount = {}
 
+#loop through each row in csv to count votes cast, define candidates as correct collumn
     for row in csvreader:
         totalvotes += 1
         candidate = row[2]
         votelist.append(candidate)
+        #if candidate not already on list then add to list when name changes
         if candidate not in candidatelist:
             candidatelist.append(candidate)
-    candidatecount = len(candidatelist)
-
+            #change candidates count to zero to loop through next person at zero
+            votecount[candidate] = 0
+            #add 1 vote per every time candidate is named in row
+        votecount[candidate] = votecount[candidate] + 1
+        #find votes specifically for one candidate and then find the percent of the total vote the candidate one
+    for vote in votecount:
+        count = votecount[vote]
+        countpercent = (count/totalvotes) *100
+        #print vote percent for candidate
+        print(f"vote percent {countpercent} : candidate name {vote}")
+    #print candidate list
     print(candidatelist)
+    #print total votes cast
     print(totalvotes)
-    print(candidatecount)
-
-    candidates = list()
-    for i in votelist:
-        name == candidatelist[i]
-        candidates.append(votelist.count(name))
-
-
-''' votes = 0
-    for index in votelist:
-        if votelist[index] != votelist[index+1]:
-            print(votes)
-            votes = 0
-        elif votelist[index] == votelist[index+1]:
-            votes += 1'''
+    #print vote per person
+    print(votecount)
