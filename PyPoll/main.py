f23bb2286baeab106d43ed869148d4dcb01b57fa
jrueve <141786894+jrueve@users.noreply.github.com>
2024-08-16 23:38:52 -0500

    new commit

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -11,6 +11,7 @@ with open(csvpath, newline="", encoding='utf-8') as csvfile:
     csv_header = next(csvreader)
     #print(f"CSV Header: {csv_header}")
 
+#define empty variables to be filled later
     totalvotes = 0
     candidatelist = []
     votelist = []
@@ -18,8 +19,11 @@ with open(csvpath, newline="", encoding='utf-8') as csvfile:
 
 #loop through each row in csv to count votes cast, define candidates as correct collumn
     for row in csvreader:
+        #count each row to get total votes
         totalvotes += 1
+        #define candidate row
         candidate = row[2]
+        #add every name in row to list to be counted and referred to later
         votelist.append(candidate)
         #if candidate not already on list then add to list when name changes
         if candidate not in candidatelist:
@@ -28,15 +32,26 @@ with open(csvpath, newline="", encoding='utf-8') as csvfile:
             votecount[candidate] = 0
             #add 1 vote per every time candidate is named in row
         votecount[candidate] = votecount[candidate] + 1
-        #find votes specifically for one candidate and then find the percent of the total vote the candidate one
+
+    #create list of percentages for each candidate to be stored in
+    percentlist = []
+    #loop through votes for each candidate in votecount
     for vote in votecount:
+        #make count equal to value of key, which is candidate name in votecount 
         count = votecount[vote]
+        #get percent votes for each candidate based on number of votes divided by total votes and multiplied by 100 for percent
         countpercent = (count/totalvotes) *100
+        #add percentages to list to reference later
+        percentlist.append(countpercent)
+
         #print vote percent for candidate
-        print(f"vote percent {countpercent} : candidate name {vote}")
-    #winning candidate finder
+        print(f"vote percent {round(countpercent,3)} : candidate name {vote}")
+
+    #winning candidate finding number of votes
     winner = max(votecount.values())
-    winpercent = round((winner/totalvotes)*100,2)
+    #round the percent
+    winpercent = round((winner/totalvotes)*100,3)
+    #find name of winner
     win = [key for key in votecount if votecount[key] == winner]
     #print(str(win))
 
@@ -47,15 +62,16 @@ with open(csvpath, newline="", encoding='utf-8') as csvfile:
     #print vote per person
     print(votecount)
 
-file_path = pathlib.Path("output_file.txt")
+#print to text file all our results in string
+file_path = pathlib.Path("Analysis","output_file.txt")
 with file_path.open( "w") as outputfile:
     print("Election Results",file=outputfile)
     print("---------------------------------",file=outputfile)
     print(f"Total Votes : {totalvotes }",file=outputfile)
     print("---------------------------------",file=outputfile)
-    print(f'Charles Casper Stockham : {countpercent}% ({candidatelist[0]})',file=outputfile)
-    print(f'Diana DeGette : {countpercent}% ({candidatelist[1]})',file=outputfile)
-    print(f'Raymon Anthony Doane : {countpercent}% ({candidatelist[2]})',file=outputfile)
+    print(f'Charles Casper Stockham : {round(percentlist[0],3)}% ({votecount.get("Charles Casper Stockham")})',file=outputfile)
+    print(f'Diana DeGette : {round(percentlist[1],3)}% ({votecount.get("Diana DeGette")})',file=outputfile)
+    print(f'Raymon Anthony Doane : {round(percentlist[2],3)}% ({votecount.get("Raymon Anthony Doane")})',file=outputfile)
     print("---------------------------------",file=outputfile)
-    print(f'Winner :  {winner} {str(win)}',file=outputfile)
+    print(f'Winner :   {str(win)}',file=outputfile)
     print("---------------------------------",file=outputfile)
